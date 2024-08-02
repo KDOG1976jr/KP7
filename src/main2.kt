0a0a659c22065c95e06f627d5923326b6806d81d
Keith Owens Jr. <owenskei000@warren.k12.in.us>
2024-08-02 12:26:44 -0400

    Initial commit

---
+++ src/main2.kt
@@ -0,0 +1,20 @@
+//part 2
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = add(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+}
+
+// Define add() function below this line
+fun add(firstNumber : Int, secondNumber : Int) : Int{
+    return firstNumber + secondNumber
+}
+fun subtract(firstNumber: Int, secondNumber: Int) : Int{
+    return firstNumber - secondNumber
+}
